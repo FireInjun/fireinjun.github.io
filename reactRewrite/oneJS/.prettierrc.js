c1c205fafc5aab984608ae95f11a668437bf6bcd
FireInjun <mk@fireinjun.com>
2019-01-25 10:09:32 -0500

    renamed to oneJS to simplify differentiation

---
+++ reactRewrite/oneJS/.prettierrc.js
@@ -0,0 +1,28 @@
+export.modules = {
+  'printWidth': 90,
+  'tabWidth': 2,
+  'useTabs': false,
+  'semi': false,
+  'singleQuote': true,
+  'trailingComma': 'none',
+  'bracketSpacing': true,
+  'jsxBracketSameLine': false,
+  'arrowParens': 'avoid'
+}
+
+// commit-msg
+// pre-push
+// post-checkout
+
+// ! Below is a list of all available scripts
+//"start": "react-scripts start",
+//"build": "react-scripts build",
+//"test": "react-scripts test",
+//"eject": "react-scripts eject",
+//"check": "gts check",
+//"clean": "gts clean",
+//"compile": "tsc -p .",
+//"fix": "gts fix",
+//"prepare": "npm run compile",
+//"pretest": "npm run compile",
+//"posttest": "npm run check"
