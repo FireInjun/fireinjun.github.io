a618df3d49fba7a5c2e0184e3c15be9a0e5ffc79
FireInjun <mk@fireinjun.com>
2019-01-25 10:26:54 -0500

    Removed files from old location

--- reactRewrite/myketwo/.lint-staged.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'lint-staged': {
-    '*.{ts,js,tsx,json}': [
-      'npm run clean', 'npm run check', 'npm run compile', 'npm run test',
-      'git add'
-    ]
-  }
-};
-// currently in planning
