c85e688de57cbcb8f43b4010a06fff4f4d093346
FireInjun <mk@fireinjun.com>
2019-01-24 23:07:09 -0500

    Added husky config

---
+++ reactRewrite/myketwo/.huskyrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  hooks: {
+    "pre-commit": "lint-staged"
+  }
+};
+
+// commit-msg
+// pre-push
+// post-checkout
+
+// ! Below is a list of all available scripts
+//"start": "react-scripts start",
+//"build": "react-scripts build",
+//"test": "react-scripts test",
+//"eject": "react-scripts eject",
+//"check": "gts check",
+//"clean": "gts clean",
+//"compile": "tsc -p .",
+//"fix": "gts fix",
+//"prepare": "npm run compile",
+//"pretest": "npm run compile",
+//"posttest": "npm run check"
